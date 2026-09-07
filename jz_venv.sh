91a388ca996ed430e1cdd222db985cb854b2364b
Abhishek Purandare <abhishek.purandare@inria.fr>
2026-09-07 17:55:33 +0200

    local jz venv working

--- jz_venv.sh
+++ jz_venv.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
+# update jax -> jax[cuda13-local] first
+
 rm -rf .venv/
 module purge
 module load arch/h100
 module load pytorch-gpu/py3/2.13.0
-python3 -v venv .venv
+
+python3 -m venv .venv
 source .venv/bin/activate
-python3 -m pip install -U pip
-python3 -m pip install --no-cache-dir  \
-  --no-binary mpi4py \
-  -r requirements.txt \
-  -r requirements_jax.txt  # update jax -> jax[cuda13-local] 
+
+python3 -m pip install -U pip setuptools wheel cython
+python3 -m pip install --no-binary mpi4py -r requirements.txt -r requirements_jax.txt 2>/dev/null || true
+python3 -m pip install --no-cache-dir --no-build-isolation --no-deps -e .
--- setup.py
+++ setup.py
@@ -58,20 +58,6 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 def parse_requirements(reqfile):
     requirements = []
-
-    with open(os.path.join(here, reqfile), encoding="utf-8") as f:
-        for line in f:
-            line = line.strip()
-            parsed = re.match(r"(\w+)(.*?)(;.*)?$", line)
-            pkg, deps, extra = parsed.groups()
-            if extra is None:
-                extra = ""
-            deps = deps.replace("==", "<=")
-            if pkg in MINIMUM_VERSIONS:
-                deps = f"{deps},>={MINIMUM_VERSIONS[pkg]}"
-            line = "".join([pkg, deps, extra])
-            requirements.append(line)
-
     return requirements
 
 
